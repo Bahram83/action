13ab54b48a06f27bf24abbb179278ef10f99db8f
Bahram83 <66073123+Bahram83@users.noreply.github.com>
2023-07-30 20:06:51 -0400

    Initialize actions

---
+++ pkg/cmd/job/view/view_test.go
@@ -0,0 +1,14 @@
+package view
+
+import "testing"
+
+func TestNewCmdView(t *testing.T) {
+	// TODO prompt
+	// TODO --log
+	// TODO showprogress
+}
+
+func TestRunView(t *testing.T) {
+	// TODO interactive flow
+	// TODO --log
+}
