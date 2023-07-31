428aaad0cdc68801414cfcb31082911f6541848c
Bahram83 <66073123+Bahram83@users.noreply.github.com>
2023-07-30 20:06:46 -0400

    Initialize cancel-error-status

---
+++ pkg/cmdutil/errors.go
@@ -0,0 +1,30 @@
+package cmdutil
+
+import (
+	"errors"
+
+	"github.com/AlecAivazis/survey/v2/terminal"
+)
+
+// FlagError is the kind of error raised in flag processing
+type FlagError struct {
+	Err error
+}
+
+func (fe FlagError) Error() string {
+	return fe.Err.Error()
+}
+
+func (fe FlagError) Unwrap() error {
+	return fe.Err
+}
+
+// SilentError is an error that triggers exit code 1 without any error messaging
+var SilentError = errors.New("SilentError")
+
+// CancelError signals user-initiated cancellation
+var CancelError = errors.New("CancelError")
+
+func IsUserCancellation(err error) bool {
+	return errors.Is(err, CancelError) || errors.Is(err, terminal.InterruptErr)
+}
